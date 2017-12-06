c20d84e5be126126f21c2fc8f0bf77c2d9ba7815
KhalDrogo5190 <31477732+KhalDrogo5190@users.noreply.github.com>
2017-12-06 09:26:34 -0500

    Add files via upload

---
+++ file_reader_puzzle_getter.py
@@ -0,0 +1,28 @@
+import os
+import random
+
+path = "data"
+
+file_names = os.listdir(path)
+
+for i,f in enumerate(file_names):
+    print(str(i) + ") " + f)
+
+print()
+choice = input("pick one: ")
+choice = int(choice)
+
+file = path + "/" + file_names[choice]
+print(file)
+
+with open(file, 'r') as f:
+    lines = f.read().splitlines()
+
+print(lines)
+
+category_name = lines[0]
+puzzle = random.choice( lines[1:] )
+
+print()
+print(category_name)
+print(puzzle)
